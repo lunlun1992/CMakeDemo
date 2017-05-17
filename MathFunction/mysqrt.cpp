4384a78f0a9c0e4417a750f4a59497fbbc0210e8
lunlun1992 <lunlun1992@163.com>
2017-05-17 17:24:13 +0800

    change CMakeLists

---
+++ MathFunction/mysqrt.cpp
@@ -0,0 +1,5 @@
+#include <math.h>
+double mysqrt(double input)
+{
+    return 1.52311;
+}
--- tutorial.cpp
+++ tutorial.cpp
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include "TutorialConfig.h"
 
+#ifdef USE_MYMATH
+#include "MathFunction.h"
+#endif
+
 int main(int argc, char *argv[])
 {
     if(argc < 2)
@@ -12,7 +16,11 @@ int main(int argc, char *argv[])
     }
 
     double input = atof(argv[1]);
+#ifdef USE_MYMATH
+    double output = mysqrt(input);
+#else
     double output = sqrt(input);
+#endif
     fprintf(stdout, "The root of %g is %g\n", input, output);
     return 0;
 }
